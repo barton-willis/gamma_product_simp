b6f7d6df1fad15507a2645333797d695364d803c
Barton Willis <willisb@unk.edu>
2022-07-22 15:53:15 -0500

    Update op_predicates.lisp
    New file: expose the CL functions mexpt, mtimesp, and mexptp to user-level

--- op_predicates.lisp
+++ op_predicates.lisp
@@ -1,3 +1,13 @@
+/* Author: Barton Willis, copyright 2022
+
+Maxima code for simplifying expressions that involve products of
+gamma functions.
+
+This is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License GPLv3; see
+https://www.gnu.org/licenses/quick-guide-gplv3.html .
+*/
+
 (defmfun $mtimesp (e) (mtimesp e))
 
 (defmfun $mplusp (e) (mplusp e))
