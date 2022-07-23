42585c098e4e674ff456204091960f17f3df8d4f
Barton Willis <willisb@unk.edu>
2022-07-23 07:19:47 -0500

    New function gamma_arg_decrease
    o Change name gamma_arg_simp to gamma_arg_increase.
    
    o New function gamma_arg_decrease.  Include this function in the top level code of gamma_product_simp.
    
    o Locally set keepfloat to true in gamma_product_simp
    
    o Correct a spelling error
    
    o Fix an expected test result.
    
    o New experimental function mymember

--- op_predicates.lisp
+++ op_predicates.lisp
@@ -1,14 +1,18 @@
-;;;;Author: Barton Willis, copyright 2022
+;;;; Author: Barton Willis, copyright 2022
 
-;;;;Maxima code for simplifying expressions that involve products of
-;;;;gamma functions.
+;;;; Maxima code for simplifying expressions that involve products of
+;;;; gamma functions.
 
-;;;;This is free software; you can redistribute it and/or
-;;;;modify it under the terms of the GNU General Public License GPLv3; see
-;;;;https://www.gnu.org/licenses/quick-guide-gplv3.html
+;;;; This is free software; you can redistribute it and/or
+;;;; modify it under the terms of the GNU General Public License GPLv3; see
+;;;; https://www.gnu.org/licenses/quick-guide-gplv3.html
+
 
 (defmfun $mtimesp (e) (mtimesp e))
 
 (defmfun $mplusp (e) (mplusp e))
 
 (defmfun $mexptp (e) (mexptp e))
+
+(defmfun $mymember (x e) (not (null 
+   (member x (cdr e) :test #'(lambda (x q) (eq t (meqp x q)))))))
