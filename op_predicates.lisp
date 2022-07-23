132847eaad2b1ce2a97a4adf6dd66bdc48f0b755
Barton Willis <willisb@unk.edu>
2022-07-22 21:17:49 -0500

    Update op_predicates.lisp
    o Fix source code comment

--- op_predicates.lisp
+++ op_predicates.lisp
@@ -1,12 +1,11 @@
-/* Author: Barton Willis, copyright 2022
+;;;;Author: Barton Willis, copyright 2022
 
-Maxima code for simplifying expressions that involve products of
-gamma functions.
+;;;;Maxima code for simplifying expressions that involve products of
+;;;;gamma functions.
 
-This is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License GPLv3; see
-https://www.gnu.org/licenses/quick-guide-gplv3.html .
-*/
+;;;;This is free software; you can redistribute it and/or
+;;;;modify it under the terms of the GNU General Public License GPLv3; see
+;;;;https://www.gnu.org/licenses/quick-guide-gplv3.html
 
 (defmfun $mtimesp (e) (mtimesp e))
 
