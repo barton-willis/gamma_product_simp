feb96419f926242904f982696d9ca66f81f50a2e
Barton Willis <willisb@unk.edu>
2022-07-31 07:58:36 -0500

    Update operator_p.lisp
    o fix major flub--used Maxima comment, not CL comment in operator_p.lisp

--- operator_p.lisp
+++ operator_p.lisp
@@ -1,4 +1,5 @@
-/* Author: Barton Willis, copyright 2022
+#|
+Author: Barton Willis, copyright 2022
 
 Common Lisp utilities for the gamma_simp package. Possibly the writing these
 functions in CL instead of Maxima makes them slightly more efficient. The Maxima
@@ -9,10 +10,12 @@ mplusp(e) := not mapatom(e) and part(e,0) = "+";
 mexptp(e) := not mapatom(e) and part(e,0) = "^"; 
 
 See LICENSE.md for licensing information.
-*/
 
-/* Depending on the value of inflag, these functions either look at the internal
-   or displayed structure of the input. */
+
+Depending on the value of inflag, these functions either look at the internal
+or displayed structure of the input. 
+
+|#
 (defmfun $mtimesp (x) (and (not ($mapatom x)) (equal ($part x 0) "*")))
 (defmfun $mplusp  (x) (and (not ($mapatom x)) (equal ($part x 0) "+")))
 (defmfun $mexptp  (x) (and (not ($mapatom x)) (equal ($part x 0) "^")))
